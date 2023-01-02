1fc66a4d75ef9f6aa4142b9bd6d71c6d75a6e1ca
Max Brown <max.brown@ed.ac.uk>
2023-01-02 15:31:58 +0000

    Extremely basic plot from list implemented.

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,53 @@
 
 use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
 use nu_protocol::{Category, Signature, Value};
+use textplots::{Chart, Plot, Shape};
+pub struct Plotter;
 
-pub struct Plot;
-
-impl Plot {
+impl Plotter {
     // some functions to do with plot types
-    fn plot(call: &EvaluatedCall, _input: &Value) -> Result<Value, LabeledError> {
-        eprintln!("Hello world!");
-        Ok(Value::Nothing { span: call.head })
+    fn plot(&self, call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
+        // cli opts
+        // let max_x = call.get_flag("max-x")?;
+        let values = match input.as_list() {
+            Ok(v) => v,
+            Err(e) => panic!("{:?}", e),
+        };
+
+        let v: Vec<(f32, f32)> = values
+            .iter()
+            .enumerate()
+            .map(|(i, e)| (i as f32, e.as_f64().unwrap() as f32))
+            .collect();
+
+        // min/max
+        fn min_max(series: &[f32]) -> (f32, f32) {
+            let min = series
+                .iter()
+                .fold(std::f32::MAX, |accu, &x| if x < accu { x } else { accu });
+            let max = series
+                .iter()
+                .fold(std::f32::MIN, |accu, &x| if x > accu { x } else { accu });
+            (min, max)
+        }
+
+        let min_max_x = {
+            let x: Vec<f32> = v.iter().map(|e| e.0).collect();
+            min_max(&x)
+        };
+
+        let chart = Chart::new(250, 50, min_max_x.0, min_max_x.1)
+            .lineplot(&Shape::Lines(&v))
+            .to_string();
+
+        Ok(Value::String {
+            val: chart,
+            span: call.head,
+        })
     }
 }
 
-impl Plugin for Plot {
+impl Plugin for Plotter {
     // Try and keep it one command with a few flags
     fn signature(&self) -> Vec<Signature> {
         vec![Signature::build("plot")
@@ -29,7 +64,7 @@ impl Plugin for Plot {
         input: &Value,
     ) -> Result<Value, LabeledError> {
         match name {
-            "plot" => Plot::plot(call, input),
+            "plot" => self.plot(call, input),
             _ => Err(LabeledError {
                 label: "Plugin call with wrong name signature".into(),
                 msg: "the signature used to call the plugin does not match any name in the plugin signature vector".into(),
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use nu_plugin::{serve_plugin, JsonSerializer};
-use nu_plugin_plot::Plot;
+use nu_plugin_plot::Plotter;
 
 fn main() {
-    serve_plugin(&mut Plot {}, JsonSerializer {})
+    serve_plugin(&mut Plotter {}, JsonSerializer {})
 }
