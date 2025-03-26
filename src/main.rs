f9cff9f4900ab8e8f1b2410763895d3f3a63137c
kurokirasama <kurokirasama@gmail.com>
2025-03-26 10:12:34 -0300

    chore: Empty commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use nu_plugin::{serve_plugin, JsonSerializer};
+use nu_plugin::{serve_plugin, MsgPackSerializer};
 use nu_plugin_plot::PluginPlot;
 
 fn main() {
-    serve_plugin(&PluginPlot, JsonSerializer {})
+    serve_plugin(&PluginPlot {}, MsgPackSerializer {})
 }
